fcc5fe70445146610dacd798c07e26b448d60e81
John <jon.meadee@gmail.com>
2015-05-18 10:59:02 -0400

    initial

---
+++ Assets/Scripts/AutoMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoMove : MonoBehaviour {
+
+	public float acceleration;
+	public float initialSpeed;
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		initialSpeed = 0.1f;
+		speed = initialSpeed;
+	}
+
+	public void ResetSpeed () {
+		speed = initialSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (speed < 2.0f) {
+			speed += Time.deltaTime * acceleration;
+		}
+		transform.position = transform.position + speed * Camera.main.transform.forward;
+	}
+}
